4bd31a2773e868fa738f275ca9728b754912152a
guesnier <samuel.guesnier@etudiant.univ-rennes1.fr>
2019-09-12 15:28:05 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+    render(){
+        return <h1>Salut</h1>
+    }
+}
+
+render(<StorePicker />,document.querySelector("#main"));
