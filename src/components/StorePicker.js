e54b70f914e665d625577182404a8db8f3c2172a
guesnier <samuel.guesnier@etudiant.univ-rennes1.fr>
2019-09-14 13:22:55 +0200

    création des différents composants nécessaires

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,17 +1,15 @@
 import React from "react";
 
 class StorePicker extends React.Component {
-    render(){
-        return (
-
-                <form action="" className="store-selector">
-                    <h2>Please enter a store</h2>
-                    <input type="text" required placeholder="Store name" />
-                    <button type="submit">Go -></button>
-                </form>
-        ) 
-        
-    }
+  render() {
+    return (
+      <form action="" className="store-selector">
+        <h2>Please enter a store</h2>
+        <input type="text" required placeholder="Store name" />
+        <button type="submit">Go -></button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
