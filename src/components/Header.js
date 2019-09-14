eb17ec82be9c3a5ad64598d628bad01c2082c837
guesnier <samuel.guesnier@etudiant.univ-rennes1.fr>
2019-09-14 20:29:13 +0200

    rajout du router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,20 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <header className="top">
-          <h1>
-            Catch
-            <span className="ofThe">
-              <span className="of">of</span>
-              <span className="the">the</span>
-            </span>
-            day
-          </h1>
-          <h3 className="tagline">
-            <span>{this.props.tagline}</span>
-          </h3>
-        </header>
-      </React.Fragment>
-    );
-  }
-}
+const Header = props => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">of</span>
+        <span className="the">the</span>
+      </span>
+      day
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
+
 
 export default Header;
