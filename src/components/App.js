086f5eec268fdeeb8bcc22e337ac0b0ca6876d56
guesnier <samuel.guesnier@etudiant.univ-rennes1.fr>
2019-10-09 10:07:35 +0200

    rajout menu commande

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,20 @@ import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
-    fishes: []
+    fishes: {},
+    order: {}
   };
 
   addFish = fish => {
     const fishes = { ...this.state.fishes };
     fishes[`fish${Date.now()}`] = fish;
     this.setState({ fishes });
+  };
+
+  addToOrder = key => {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({order});
   };
 
   loadSampleFishes = () => {
@@ -27,15 +34,16 @@ class App extends React.Component {
           <Header tagline="Fresh Sea Food Market" />
           <ul>
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} index={key} details={this.state.fishes[key]} />
+              <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
             ))}
           </ul>
         </div>
 
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
+          
         />
       </div>
     );
